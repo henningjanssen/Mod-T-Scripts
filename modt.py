1a3b65f9fc5120f723a1131a317777db9f0d801f
Henning Janßen <development@henning-janssen.net>
2020-06-30 16:49:31 +0200

    Clear nozzle at 220deg (was 230)

--- modt.py
+++ modt.py
@@ -9,7 +9,7 @@ from zlib import adler32
 class GCODES:
     CLEAR_NOZZLE = '''
         M82
-        M109 S230.0 ;set temp and wait
+        M109 S220.0 ;set temp and wait
         G0 Z100 F300 ; move to Z 100 when @ temp
         G1 E10.0000 F9000.00000 ; extrude some filament
         G0 Z50 F100 ; move down slowly after extruding
